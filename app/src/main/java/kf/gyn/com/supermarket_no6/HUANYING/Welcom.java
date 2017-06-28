bcf13d9a55630c3215bf332bbdbd1a09a5c8572c
jerryjjh <290485788@qq.com>
2017-06-28 20:57:49 +0800

    2057

--- app/src/main/java/kf/gyn/com/supermarket_no6/HUANYING/Welcom.java
+++ app/src/main/java/kf/gyn/com/supermarket_no6/HUANYING/Welcom.java
@@ -1,6 +1,8 @@
 package kf.gyn.com.supermarket_no6.HUANYING;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * @类的用途:
@@ -9,4 +11,9 @@ import android.app.Activity;
  */
 
 public class Welcom extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
