fa78e164bcfa0b8e0484179e06a6c3c8f8f16d80
jerryjjh <290485788@qq.com>
2017-06-28 11:17:17 +0800

    jjh

--- app/src/main/java/kf/gyn/com/supermarket_no6/MainActivity.java
+++ app/src/main/java/kf/gyn/com/supermarket_no6/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //shizhuangzhuang
-       //56465465jjh
+       //56465465jjh....
 
     }
 }
